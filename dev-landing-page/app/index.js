9c1131e8908aaedcd1171d7dd6a620007c497ef1
pratikmanta <pratik.manta10@gmail.com>
2022-03-13 15:11:22 +0530

    updated experience section  animation

--- dev-landing-page/app/index.js
+++ dev-landing-page/app/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../modules/Root/root';
-
+import Root from '../modules/Root/root';
 import Profile from '../modules/Profile/profile';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
 	<Router>
-		<Route exact path='/' component={App} />
+		<Route exact path='/' component={Root} />
 		<Route exact path='/profile' component={Profile} />
 	</Router>,
 	document.getElementById('root')
